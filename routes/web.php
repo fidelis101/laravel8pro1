bb170fd9ff501227337a2dcb42d8ad970707dab9
Prime <ugwufidelis@ymail.com>
2020-12-16 21:21:56 +0100

    Test

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home',function(){return "Hello";});
+Route::get('/home/{name}',[HomeController::class,'index'])->name('home.index');
